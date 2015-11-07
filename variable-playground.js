82327552e6b9931f8efdb0e7f49a1c27ba1a577d
Guido Granobles <guido-granobles@hotmail.com>
2015-11-06 23:17:35 -0600

    Add variable playground area

---
+++ variable-playground.js
@@ -0,0 +1,18 @@
+
+var grades = [25,30,45];
+
+function updateArray(arr){
+
+	 arr = [60,30,12];
+}
+
+
+function updateArray2(arr){
+
+	  arr.push(85);
+}
+
+updateArray(grades);
+console.log(grades);
+updateArray2(grades)
+console.log(grades);
